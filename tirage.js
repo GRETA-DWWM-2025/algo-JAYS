bc3a998c7038d62f846165f85612921ba72ccaad
Chebbi <said.chebbi41@gmail.com>
2025-04-18 16:48:51 +0200

    Ajout module création de groupe en javascript

--- tirage.js
+++ tirage.js
@@ -20,6 +20,25 @@ for(i=0; i<9; i=i+1) {
 
 }
 console.log("1 -",t[0],t[1],t[2])
-
 console.log("2 -",t[3],t[4], t[5])
 console.log("3 -",t[6],t[7], t[8], t[9])
+
+function fibonacci(n) {
+    let sequence = [0, 1]; // Les deux premiers termes de la suite
+
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]); // Somme des deux précédents
+    }
+
+    return sequence;
+}
+
+console.log(fibonacci(10)); // Affiche les 10 premiers termes
+function afficherTriangle(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log('*'.repeat(i)); // Répète le caractère '*' i fois
+    }
+}
+
+afficherTriangle(5); // Affiche un triangle de 5 lignes
+
